5a18e34f9d4a2c075bdf959e39b3f6c761b50051
OnelioViera <ojvwebdesign@gmail.com>
2023-07-05 14:47:31 -0400

    updated files

--- app.py
+++ app.py
@@ -17,12 +17,27 @@ def add_task():
     return redirect("/")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 @app.route("/delete_task", methods=["POST"])
 def delete_task():
     task = request.form["task"]
     tasks.remove(task)
     return redirect("/")
+
+
+@app.route("/edit_task", methods=["POST"])
+def edit_task():
+    task = request.form["task"]
+    return render_template("edit.html", task=task)
+
+
+@app.route("/update_task", methods=["POST"])
+def update_task():
+    old_task = request.form["old_task"]
+    new_task = request.form["new_task"]
+    index = tasks.index(old_task)
+    tasks[index] = new_task
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
